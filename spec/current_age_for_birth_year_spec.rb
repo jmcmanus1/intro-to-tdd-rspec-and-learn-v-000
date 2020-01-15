e99bd2748b9c7a5241e92afdd84289c9cb645903
Julie Anne McManus <julieannesays@gmail.com>
2020-01-15 20:11:03 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -6,5 +6,8 @@ describe "current_age_for_birth_year method" do
     birth_year = 1984
     answer = current_year - birth_year
 
+    age_of_person = current_age_for_birth_year(birth_year)
+    expect(age_of_person).to eq(answer)
+
   end
 end
